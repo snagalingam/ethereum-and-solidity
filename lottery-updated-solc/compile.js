08cbb58f22f7cd5e7abb3e697fa64f3d0d3fa93a
Sinthuja Nagalingam <sinthuja@popsql.com>
2022-07-27 19:42:38 -0700

    create compile file

--- lottery-updated-solc/compile.js
+++ lottery-updated-solc/compile.js
@@ -6,21 +6,21 @@ const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 const source = fs.readFileSync(lotteryPath, 'utf8');
 
 const input = {
-    language: 'Solidity',
-    sources: {
-      'Lottery.sol': {
-        content: source,
+  language: 'Solidity',
+  sources: {
+    'Lottery.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
       },
     },
-    settings: {
-      outputSelection: {
-        '*': {
-          '*': ['*'],
-        },
-      },
-    },
-  };
+  },
+};
    
-  module.exports =∏JSON.parse(solc.compile(JSON.stringify(input))).contracts[
-    'Lottery.sol'
-  ].Lottery;
+module.exports =JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  'Lottery.sol'
+].Lottery;
